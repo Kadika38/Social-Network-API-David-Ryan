6e05701885043e89d4e1c6410832e7b49a1e8737
Kadika <dryan10101@gmail.com>
2022-08-02 13:25:04 -0700

    added get single thought func to thoughts controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -6,4 +6,13 @@ module.exports = {
           .then((thoughts) => res.json(thoughts))
           .catch((err) => res.status(500).json(err));
     },
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+          .then((thought) =>
+            !thought
+              ? res.status(404).json({ message: 'No thought with that ID' })
+              : res.json(thought)
+          )
+          .catch((err) => res.status(500).json(err));
+      },
 };
