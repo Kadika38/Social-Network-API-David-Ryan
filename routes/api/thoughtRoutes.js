09520177b95a9b2c76a05b983c905cab0891a3e2
Kadika <dryan10101@gmail.com>
2022-08-02 14:39:35 -0700

    added delete route for reactions to thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,6 +4,7 @@ const {
     getSingleThought,
     createThought,
     addReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController');
 
 router.route('/').get(getThoughts).post(createThought);
@@ -11,5 +12,7 @@ router.route('/').get(getThoughts).post(createThought);
 router.route('/:thoughtId').get(getSingleThought);
 
 router.route('/:thoughtId/react').post(addReaction);
+
+router.route('/:thoughtId/react/:reactionId').delete(deleteReaction);
 
 module.exports = router;
