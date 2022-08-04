e0e951bf08fd9d884eeaac84a0177dbb84fdff23
Kadika <dryan10101@gmail.com>
2022-08-03 17:30:49 -0700

    monir change in thought routes to match hw reqs

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -13,8 +13,8 @@ router.route('/').get(getThoughts).post(createThought);
 
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-router.route('/:thoughtId/react').post(addReaction);
+router.route('/:thoughtId/reactions').post(addReaction);
 
-router.route('/:thoughtId/react/:reactionId').delete(deleteReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
