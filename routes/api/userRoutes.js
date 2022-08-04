1f0017d70005a059612b19c7d768499b06cbca9c
Kadika <dryan10101@gmail.com>
2022-08-03 17:28:24 -0700

    minor change in user routes to match hw reqs

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,6 +13,6 @@ router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-router.route('/:userId/:friendId').post(addFriend).delete(deleteFriend);
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
