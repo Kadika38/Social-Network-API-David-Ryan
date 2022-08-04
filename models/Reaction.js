c6d479972fddd598e028e07023f9540cdc811242
Kadika <dryan10101@gmail.com>
2022-08-03 17:26:01 -0700

    redid reaction model to meet hw requirements, no major changes, just added properties and a createdAt field

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,14 +2,32 @@ const { Schema, model } = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-      reactionText: String,
-      username: String,
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: () => { return Date.now(); },
+            get: (date) => {
+                return moment(date).format("MMMM Do YYYY, h:mm a");
+            },
+        },
     },
     {
-      /* toJSON: {
-        virtuals: true,
-      }, */
-      id: true,
+        /* toJSON: {
+            virtuals: true,
+        }, */
+        id: true,
     }
   );
   
